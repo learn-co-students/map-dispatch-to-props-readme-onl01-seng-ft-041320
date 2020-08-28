eea49e70f24cf722ab94c8b5d942e027b1200c8a
leighad <cdversiontwo@gmail.com>
2020-08-27 17:39:27 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick = event => {
+    // this.props.store.dispatch(addItem());
+    this.props.addItem()
   }
 
   render() {
+    debugger 
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        {/* <button onClick={(event) => this.handleOnClick(event)}> */}
+        <button onClick={this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -27,4 +30,34 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, { addItem })(App);
+
+// NOTE 
+// export default connect(state => ({ items: state.items }), { addItem })(App);
+
+// is just like writing:
+
+// const mapStateToProps = state => {
+//   return {
+//     items: state.items
+//   }
+// }
+ 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => { dispatch(addItem()) }
+//   }
+// }
+ 
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
